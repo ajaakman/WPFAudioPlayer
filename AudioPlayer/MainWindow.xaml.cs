cab6bdfa11f99d79a7f1d5cbbd4cad06f3251d05
Artur Jaakman <jaakmanartur@gmail.com>
2019-02-01 22:35:52 +0200

    Added Assetes

--- AudioPlayer/MainWindow.xaml.cs
+++ AudioPlayer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace AudioPlayer
 {
@@ -27,7 +28,15 @@ namespace AudioPlayer
 
         void Play(object sender, RoutedEventArgs args)
         {
-            MyMediaElement.Play();
+            ToggleButton tb = (ToggleButton)sender;
+            if ((bool)tb.IsChecked)
+            {
+                MyMediaElement.Play();
+            }
+            else
+            {
+                MyMediaElement.Pause();
+            }
         }
 
     }
